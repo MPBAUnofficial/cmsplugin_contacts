69d03941113537759082ee82500da16a4d8dbfee
Luca Colleoni <luca.colle92@gmail.com>
2013-02-06 15:03:08 +0100

    increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='cmsplugin_contacts',
-        version='1.2',
+        version='1.3',
         description='Django cms plugin for insert contacts in website',
         author='Colleoni Luca for fbk',
         author_email='colleoni@fbk.eu',
